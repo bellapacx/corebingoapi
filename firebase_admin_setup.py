adc2453d28645d7d3367385bab3ad2bb6e2e5e12
Abel Aman <136691061+bellapacx@users.noreply.github.com>
2025-06-07 10:13:14 +0300

    Update firebase_admin_setup.py

--- firebase_admin_setup.py
+++ firebase_admin_setup.py
@@ -3,4 +3,5 @@ from firebase_admin import credentials, firestore
 
 cred = credentials.Certificate("/etc/secrets/serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
-db = firestore.client()
+
+db = firestore.client()  # ✅ This is the correct Firestore client
