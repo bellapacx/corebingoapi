e27c822b80505fbe103e2c92deadc614b9af877d
Abel Aman <136691061+bellapacx@users.noreply.github.com>
2025-06-12 14:29:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 origins = [
     "https://bingoapp-mocha.vercel.app",
     "https://bingoadmin-9wyy.vercel.app",
-    "https://www.halobingo.com/",
+    "https://www.halobingo.com",
     "http://localhost:5175",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
