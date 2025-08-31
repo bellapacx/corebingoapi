ec2be898ca397694cbc94a760c4d9af7df4c8cf3
sunvilacoin <sunvilacoin@gmail.com>
2025-08-31 21:36:29 +0300

    Fresh startddsdsssdddaddddkhsskkllkee

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "http://127.0.0.1:5174",
+    "https://gojobingoadmin.vercel.app"
     # add other origins if needed
 ]
 
